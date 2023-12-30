645e9c854a9fda38944d46d2b57ee2fd98e5c114
Tulsi <tulsisatasiya085@gmail.com>
2023-12-30 10:26:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Props from './components/Props';
+
+function App() {
+  return (
+    <div className="App">
+     <Props name="tulsi" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Props.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Props = (props) => {
+  return (
+    <div>
+      <h1>welcome {props.name} </h1>
+    </div>
+  )
+}
+
+export default Props
